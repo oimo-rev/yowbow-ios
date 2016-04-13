72bc42dec1731e1c4478aabe69e80e274daea3cc
oimosan <zushitomomi@FL1-122-135-187-137.tky.mesh.ad.jp>
2016-04-13 15:23:21 +0900

    all commit

---
+++ ybwTests/ybwTests.m
@@ -0,0 +1,39 @@
+//
+//  ybwTests.m
+//  ybwTests
+//
+//  Created by 図師ともみ on 2016/04/12.
+//  Copyright © 2016年 Manhattan-Code Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ybwTests : XCTestCase
+
+@end
+
+@implementation ybwTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
